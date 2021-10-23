cf68a99d32ef4131626b44f46c7ad426e34eeac6
Joao Satiro <joao.v.satiro@gmail.com>
2021-10-23 16:59:01 -0300

    Aula: Corrupção no Código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("João", "Sátiro", "1234567894611", "hello@satiro.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
